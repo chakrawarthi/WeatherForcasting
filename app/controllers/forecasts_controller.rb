328a030ddeef8ca490432e27006c3f32be1ff1a8
Clink <chakrawarthy123@gmail.com>
2025-11-26 11:06:28 +0530

    add the current location

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -9,27 +9,45 @@ class ForecastsController < ApplicationController
     redirect_to forecast_path(address: address)
   end
 
-  def show
-    address = params[:address]
+def show
+  address = params[:address]
+  lat = params[:lat]
+  lon = params[:lon]
+  if lat.present? && lon.present?
+    reverse = Geocoder.search([lat, lon]).first
+    city = reverse&.city || reverse&.data["name"] || "Unknown Location"
+    zipcode = reverse&.postal_code
+    geo = GeocodingService::Result.new(
+      true,
+      zipcode,
+      lat,
+      lon,
+      city,
+      nil
+    )
+  else
     if address.blank?
       return render plain: "Please provide an address."
     end
     geo = GeocodingService.new(address).call
     return render plain: geo.error unless geo.success?
+  end
+  key = "forecast_#{geo.zipcode || "#{geo.lat}_#{geo.lon}"}"
+  cached = Rails.cache.read(key)
 
-    key = "forecast_#{geo.zipcode || "#{geo.lat}_#{geo.lon}"}"
-    cached = Rails.cache.read(key)
- 	if cached
-  		@forecast = cached.merge(from_cache: true)
-  		return render :show
-	end
-    weather = WeatherService.new(geo.lat, geo.lon).fetch_weather
-    return render plain: weather.error unless weather.success?
-    presenter = ForecastPresenter.new(weather.payload, geo)
-    output = presenter.to_hash.merge(from_cache: false)
-    Rails.cache.write(key, output, expires_in: 30.minutes)
-    @forecast = output
-    render :show
+  if cached
+    @forecast = cached.merge(from_cache: true)
+    return render :show
   end
 
+  weather = WeatherService.new(geo.lat, geo.lon).fetch_weather
+  return render plain: weather.error unless weather.success?
+
+  presenter = ForecastPresenter.new(weather.payload, geo)
+  output = presenter.to_hash.merge(from_cache: false)
+  Rails.cache.write(key, output, expires_in: 30.minutes)
+  @forecast = output
+
+  render :show
+end
 end
